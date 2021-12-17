502dc634ea4a6449da10629555c9c133baf97f8a
nakio17 <you@example.com>
2021-12-17 10:47:03 +0700

    danh sach

---
+++ app/src/main/java/com/example/qlsd/DanhSach.java
@@ -0,0 +1,14 @@
+package com.example.qlsd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DanhSach extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach);
+    }
+}
